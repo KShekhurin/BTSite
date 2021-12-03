f365551dc816c6b603892b35c09db0157b282989
KShek <36967908+KShekhurin@users.noreply.github.com>
2021-12-03 23:50:41 +0300

    Time update

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+var k = 0
+
 function ready() {
     setInterval(stroka, 500)
 }
-    
+
 function stroka() {
     var h = document.getElementsByClassName("title")[0];
-    h.textContent = h.textContent.substr(1, h.textContent.length-1) + h.textContent[0];
+    if (h.textContent[0] == "К" && k < 2) {
+        k++;
+        return;
+    } else {
+        h.textContent = h.textContent.substr(1, h.textContent.length-1) + h.textContent[0];
+        k = 0;
+    }
 }
-    
-document.addEventListener("DOMContentLoaded", ready)
+
+document.addEventListener("DOMContentLoaded", ready);
